ad21674f29c9256fea7f319eacdb6021de0de068
John Coffelt <jcoffelt@csumb.edu>
2018-07-10 21:52:50 -0700

    Lab5

---
+++ Lab5/dbConnection.php
@@ -0,0 +1,17 @@
+<?php
+
+function getDatabaseConnection($dbname){
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    
+    //creates db connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    //display errors when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}
+
+?>
